38878334eb30747402e303c1d0be90e43f917eb9
carlosep93 <carlos.e.p93@gmail.com>
2025-04-17 12:54:43 +0200

    salamandra-instruct

--- run_local.sh
+++ run_local.sh
@@ -1,4 +1,5 @@
 export MT_API_CONFIG=config.json
 export MODELS_ROOT=./models
 python -m nltk.downloader -d . punkt
-python main.py --models './models'
+#python main.py --models './models' --load MULTI-MULTI-salamandra-instruct 
+python main.py  --load MULTI-MULTI-salamandra-instruct 
